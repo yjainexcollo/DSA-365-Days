8e46f3a6ac41e9426107c55e880858628e1b2330
yashjain3010 <yashjain30102000@gmail.com>
2025-11-10 06:59:44 +0530

    Range Sum Query

---
+++ Prefix Sum/Range Sum Query.cpp
@@ -0,0 +1,17 @@
+class NumArray {
+public:
+    vector<int> a;
+    NumArray(vector<int>& nums) {
+        a.push_back(nums[0]);
+        for(int i = 1 ; i < nums.size() ; i++){
+            a.push_back(a[i-1]+nums[i]);
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        if(left == 0){
+            return a[right];
+        }
+        return a[right]-a[left-1];
+    }
+};
